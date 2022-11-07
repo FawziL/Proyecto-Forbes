637aa64c985eed46acf4ab6a63820978798f03e3
FawziLutfallah <fawzi@gmail.com>
2022-11-07 13:59:37 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+    const contenedorProductos = document.querySelector("#productos");
+    const contenido = document.getElementById('contenido');
+    const botones = document.getElementById('botones');
+    const stock = "js/stock.json"
+
+
+fetch(stock)
+.then(respuesta => respuesta.json())
+.then(productos =>{
+
+    productos.forEach(producto =>{
+
+         const casilla = document.createElement('section');
+
+         const imagenProducto = document.createElement('img');
+         imagenProducto.classList.add('imgProduct')
+         imagenProducto.src = producto.img;
+         imagenProducto.onclick =  () => {
+            hola()
+         };
+
+         const tituloProducto = document.createElement('h3');
+         tituloProducto.textContent = producto.nombre;
+
+         const precioProducto = document.createElement('h4');
+         precioProducto.textContent = producto.precio  + "$";
+
+         const descripcionProducto = document.createElement('h5')
+         descripcionProducto.textContent = producto.descripcion;
+
+         const btnFavorito = document.createElement('button');
+         btnFavorito.textContent = "Ver más detalles";
+         btnFavorito.onclick =  () => {
+            agregarAFavorito(producto.id)
+         };
+
+         casilla.appendChild(imagenProducto);
+         casilla.appendChild(tituloProducto);
+         casilla.appendChild(precioProducto);
+         casilla.appendChild(descripcionProducto);
+         casilla.appendChild(btnFavorito);
+         
+         contenedorProductos.appendChild(casilla)
+
+});
+
+function hola(){
+    console.log("hola")
+}
+})
+
+   
