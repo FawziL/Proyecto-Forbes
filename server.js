b7ee82f11d6199486e79113fcfb0e0d54db0c9be
FawziLutfallah <fawzi@gmail.com>
2022-11-25 23:42:01 -0400

    make it better

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { Server: HttpServer } = require('http')
 const app = express()
 const httpServer = new HttpServer(app)
+const puerto = process.env.PORT || 8080;
 const rutas = require('./routes/index')
 
 app.use(express.json())
@@ -9,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/forbes', rutas)
 app.use(express.static('public'))
 
-const connectedServer = httpServer.listen(8080, () => {
+const connectedServer = httpServer.listen(puerto, () => {
     console.log(`Servidor escuchando en el puerto: ${connectedServer.address().port}`)
 })
 connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
